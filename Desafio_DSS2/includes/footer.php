ecae9010e2a365dd4d47f5f670d39a73fe8867eb
EduardoRamirez86 <eduardoalfredo124@gmail.com>
2025-04-11 05:19:17 -0600

    estilo

--- Desafio_DSS2/includes/footer.php
+++ Desafio_DSS2/includes/footer.php
@@ -1,5 +1,8 @@
 </div> <!-- Cierre del container -->
     <script src="../assets/js/jquery.min.js"></script>
     <script src="../assets/js/bootstrap.min.js"></script>
+<footer>
+    <p>&copy; <?php echo date('Y'); ?> Mi Tienda. Todos los derechos reservados.</p>
+</footer>
 </body>
 </html>
